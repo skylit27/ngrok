94688f5409c0277ef566d55b7d03ab3ae151581b
skylit <cesaralonso227@gmail.com>
2021-09-22 14:07:57 -0500

    modificaion en la clase test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,11 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	String meessage="default message cambio test devops";
+	Assert.assertNotNull(meessage);
+
 	}
 
 }
